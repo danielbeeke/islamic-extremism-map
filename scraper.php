9ff22a5cef6732dd0b2c3a07c028eb1160a7404f
Daniel Beeke <daniel@studiofonkel.nl>
2016-07-16 14:52:24 +0200

    Parser is working a bit

--- scraper.php
+++ scraper.php
@@ -1,21 +1,56 @@
 <?php
 
-include('simple_html_dom.php');
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+libxml_use_internal_errors(true);
 
 $years = [2011, 2012, 2013, 2014, 2015, 2016];
+
+if (!is_dir('.tmp')) {
+    mkdir('.tmp');
+}
 
 foreach ($years as $year) {
     if (!file_exists('.tmp/' . $year . '.cache')) {
         $year_data = file_get_contents('http://www.thereligionofpeace.com/attacks/attacks.aspx?Yr=' . $year);
-        file_put_contents('.tmp/' . $year . '.cache', $year_data);
+
+        $year_data_exploded = explode('<!-- Begin Attacks Table-->', $year_data);
+        $year_data_content_plus_broken_footer_exploded = explode('</table>', $year_data_exploded[1]);
+
+        $cleaned_data = $year_data_content_plus_broken_footer_exploded[0] . '</table>';
+
+        file_put_contents('.tmp/' . $year . '.cache', $cleaned_data);
     }
 }
 
 foreach ($years as $year) {
-    $html = file_get_html('.tmp/' . $year . '.cache');
+    $file_contents = '<html><body>' . file_get_contents('.tmp/' . $year . '.cache') . '</body></html>';
+    $objects = [];
 
-    foreach($html->find('div.post div.centre h3') as $e) {
-        foreach($e->find("a") as $a) $text = $a->innertext;
-        foreach($e->find("a") as $a) $link = $a->href;
+    $doc = new DOMDocument();
+    $doc->loadHTML($file_contents);
+
+    $delta_mapping = [
+        0 => 'date',
+        2 => 'country',
+        4 => 'city',
+        6 => 'killed',
+        8 => 'injured',
+        10 => 'description'
+    ];
+
+    foreach($doc->getElementsByTagName('tr') as $row) {
+        $row_object = [];
+
+        foreach ($row->childNodes as $delta => $td) {
+            if (isset($delta_mapping[$delta])) {
+                $row_object[$delta_mapping[$delta]] = trim($td->nodeValue);
+            }
+        }
+
+        $objects[] = $row_object;
     }
+
+    print_r($objects);
 }
