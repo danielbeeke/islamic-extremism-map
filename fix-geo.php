ed9bb5b477013e7620ffa508859f9987a5f363a9
Daniel Beeke <daniel@studiofonkel.nl>
2016-07-26 23:50:52 +0200

    db is full geocoded

--- fix-geo.php
+++ fix-geo.php
@@ -8,7 +8,8 @@ libxml_use_internal_errors(true);
 define('CARTO_APIKEY', '7a21536f8d4e92fae1a647e6bf47355b95d49df3');
 define('CARTO_URL', 'http://danielbeeke.carto.com/api/v2/sql?q=');
 
-$items_to_geocode_query = 'SELECT * FROM islamic_extremism WHERE cartodb_georef_status = false ORDER BY cartodb_id ASC LIMIT 100';
+$items_to_geocode_query = 'SELECT * FROM islamic_extremism WHERE the_geom IS NULL ORDER BY cartodb_id ASC LIMIT 3000';
+
 $items_to_geocode = json_decode(file_get_contents(CARTO_URL . urlencode($items_to_geocode_query)), TRUE);
 $geo_cache = json_decode(file_get_contents('data_cache/geo.cache'), TRUE);
 
@@ -20,6 +21,53 @@ foreach ($items_to_geocode['rows'] as $object) {
         $object['geo'] = $geo_cache[$object['country']]['COUNTRY'];
     }
     else {
-        print 'Could not find: ' . $object['city'] . ' in ' . $object['country'];
+        print 'Could not find: ' . $object['city'] . ' in ' . $object['country'] . ' trying google.';
+
+        $location = $object['city'] . ',' . $object['country'];
+        $location = urlencode(cleanString($location));
+        $geo_result = json_decode(file_get_contents('https://maps.google.com/maps/api/geocode/json?key=AIzaSyBbvfdgUmI8t8289qFA-_o5yw4har1F0g8&address=' . cleanString($object['city']) . '&components=country:' . cleanString($object['country'])), TRUE);
+
+        if (isset($geo_result['results'][0]['geometry']['location'])) {
+            $object['geo'] = $geo_result['results'][0]['geometry']['location'];
+        }
+    }
+
+    if (isset($object['geo'])) {
+        $query = urlencode('UPDATE islamic_extremism ' .
+                'SET the_geom = ST_GeomFromText(\'POINT(' . $object['geo']['lng'] . ' ' . $object['geo']['lat'] . ')\', 4326) ' .
+                'WHERE cartodb_id = ' . $object['cartodb_id']);
+
+        $api = CARTO_URL . $query . '&api_key=' . CARTO_APIKEY;
+
+        print $api . "\n\n";
+
+        file_get_contents($api);
     }
 }
+
+
+function cleanString($text) {
+    $utf8 = array(
+        '/[áàâãªäa]/u'   =>   'a',
+        '/[ÁÀÂÃÄ]/u'    =>   'A',
+        '/[ÍÌÎÏ]/u'     =>   'I',
+        '/[íìîï]/u'     =>   'i',
+        '/[éèêë]/u'     =>   'e',
+        '/[ÉÈÊË]/u'     =>   'E',
+        '/[óòôõºö]/u'   =>   'o',
+        '/[ÓÒÔÕÖ]/u'    =>   'O',
+        '/[úùûü]/u'     =>   'u',
+        '/[ÚÙÛÜ]/u'     =>   'U',
+        '/ç/'           =>   'c',
+        '/Ç/'           =>   'C',
+        '/ñ/'           =>   'n',
+        '/Ñ/'           =>   'N',
+        '/–/'           =>   '-', // UTF-8 hyphen to "normal" hyphen
+        '/[’‘‹›‚]/u'    =>   ' ', // Literally a single quote
+        '/[“”«»„]/u'    =>   ' ', // Double quote
+        '/  /'           =>   ' ', // nonbreaking space (equiv. to 0x160)
+    );
+    return urlencode(preg_replace(array_keys($utf8), array_values($utf8), $text));
+}
+
+
